da4dc05b90527cfd325ccbffa79e5b7010623cf4
OgaDavid <oluwademiladedavidoga@gmail.com>
2025-12-16 22:20:07 +0100

    optimize  code

--- recursion/binary-search.go
+++ recursion/binary-search.go
@@ -1,28 +1,48 @@
 package main
 
-func BinarySearch(arr []int, n int) int {
-	arrlen := len(arr)
-	mid := arrlen / 2
+// we didn't have to slice arrays at all we can just swap
+// starting and end positions and solve recursively
 
-	var tempArr []int
-
-	if arrlen == 0 {
+func BinarySearch(arr []int, target, start, end int) int {
+	if start > end {
 		return -1
 	}
 
-	if arr[mid] != n {
-		if arr[mid] < n {
-			tempArr = arr[mid+1:]
-			result := BinarySearch(tempArr, n)
-			if result == -1 {
-				return -1
-			}
-			return (mid + 1) + BinarySearch(tempArr, n)
-		} else {
-			tempArr = arr[:mid]
-			return BinarySearch(tempArr, n)
-		}
+	m := start + (end-start)/2
+	if arr[m] == target {
+		return m
 	}
 
-	return mid
+	if target < arr[m] {
+		return BinarySearch(arr, target, start, m-1)
+	}
+
+	return BinarySearch(arr, target, m+1, end)
 }
+
+// func BinarySearch(arr []int, n int) int {
+// 	arrlen := len(arr)
+// 	mid := arrlen / 2
+
+// 	var tempArr []int
+
+// 	if arrlen == 0 {
+// 		return -1
+// 	}
+
+// 	if arr[mid] != n {
+// 		if arr[mid] < n {
+// 			tempArr = arr[mid+1:]
+// 			result := BinarySearch(tempArr, n)
+// 			if result == -1 {
+// 				return -1
+// 			}
+// 			return (mid + 1) + BinarySearch(tempArr, n)
+// 		} else {
+// 			tempArr = arr[:mid]
+// 			return BinarySearch(tempArr, n)
+// 		}
+// 	}
+
+// 	return mid
+// }
--- recursion/main.go
+++ recursion/main.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	arr := []int{2, 5, 8, 12 ,16, 23, 38, 56, 72, 91}
-	
+	arr := []int{2, 5, 8, 12, 16, 23, 28, 56, 72, 91}
+
 	// fmt.Println(Fibonacci(7))
-	fmt.Println(BinarySearch(arr, 28))
+	fmt.Println(BinarySearch(arr, 28, 0, len(arr)-1))
 }
